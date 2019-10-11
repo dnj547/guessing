079a5672bef3e0bae3c86d3ca6ee594d599c5fba
dnj547 <dnj547@live.com>
2019-10-11 16:57:47 -0400

    Show blanks for each letter

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import Blank from './Components/Blank';
 import './App.css';
 
 function App() {
@@ -18,9 +19,14 @@ function App() {
       setWord(words[Math.floor(Math.random()*words.length)])
     }
   }, [words])
+  const blanks = word ? word.split("").map((letter, i) => {
+    return <Blank letter={letter} key={i}/>
+  }) : null
   return (
     <div className="App">
       Guesses Remaining: {guessesLeft}
+      <br/>
+      Word: {blanks}
     </div>
   );
 }
---
+++ src/Components/Blank.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Blank (props) {
+  console.log(props);
+  return "_ "
+}
+export default Blank;
