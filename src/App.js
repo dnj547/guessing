275396b56b1137b8b34cb43f27bbcecf753b9fec
dnj547 <dnj547@live.com>
2019-10-16 11:03:30 -0400

    Fix exes turning into letters and number of guesses left

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import Game from './Components/Game';
 import Intro from './Components/Intro';
-import Lost from './Components/Lost';
-import Won from './Components/Won';
+// import Lost from './Components/Lost';
+// import Won from './Components/Won';
 import './App.css';
 
 function App() {
@@ -26,14 +26,11 @@ function App() {
   const [difficultyNum, setDifficultyNum] = useAsyncState(0);
 
   // normal react hooks
-  const [correct, setCorrect] = useState([]);
-  const [incorrect, setIncorrect] = useState([]);
-  const [guess, setGuess] = useState('');
-  const [guessesLeft, setGuessesLeft] = useState(6);
-  const [gameState, setGameState] = useState('intro');
+
+  const [introScreen, setIntroScreen] = useState(true);
+  const [gameScreen, setGameScreen] = useState(false);
   const [words, setWords] = useState([]);
   const [word, setWord] = useState('');
-  const [alert, setAlert] = useState('');
 
   // set new word whenever words array changes
   useEffect(() => {
@@ -57,43 +54,32 @@ function App() {
     newGame(newDifficultyNum);
   };
 
-  const reset = () => {
+  const resetApp = () => {
     setDifficulty('none');
     setDifficultyNum(0);
     setWord(words[Math.floor(Math.random()*words.length)]);
-    setGuessesLeft(6);
-    setCorrect([]);
-    setIncorrect([]);
-    setGameState('intro');
+    setGameScreen(false);
+    setIntroScreen(true);
   }
 
   const newGame = (newDifficultyNum) => {
     fetch(PROXY+API+`?difficulty=${newDifficultyNum}&minLength=3&maxLength=10`)
       .then(res => res.text())
       .then(words => setWords(words.split(`\n`)))
-      .then(nothing => setGameState('game'));
+      .then(nothing => {
+        setIntroScreen(false);
+        setGameScreen(true);
+      });
   }
 
   return (
     <div className="App">
       <h1>Ghoul Be Gone</h1>
-      {gameState === 'intro' ? <Intro handleClick={handleClick} /> : null}
-      {gameState === 'lost' ? <Lost reset={reset} /> : null}
-      {gameState === 'won' ? <Won reset={reset} /> : null}
-      {gameState === 'game' ? <Game
-        setGameState={setGameState}
+      {introScreen ? <Intro handleClick={handleClick} /> : null}
+      {gameScreen ? <Game
         word={word}
         setWord={setWord}
-        guess={guess}
-        setGuess={setGuess}
-        incorrect={incorrect}
-        setIncorrect={setIncorrect}
-        correct={correct}
-        setCorrect={setIncorrect}
-        guessesLeft={guessesLeft}
-        setGuessesLeft={setGuessesLeft}
-        alert={alert}
-        setAlert={setAlert} /> : null}
+        resetApp={resetApp} /> : null}
     </div>
   );
 }
--- src/Components/Game.js
+++ src/Components/Game.js
@@ -1,72 +1,98 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Blank from './Blank';
 import Ex from './Ex';
 import Alert from './Alert';
 
 function Game (props) {
+  const [guess, setGuess] = useState('');
+  const [correct, setCorrect] = useState([]);
+  const [incorrect, setIncorrect] = useState([]);
+  const [guessesLeft, setGuessesLeft] = useState(6);
+  const [alert, setAlert] = useState('');
+  const [gameOver, setGameOver] = useState(false);
+
   const blanks = props.word ? props.word.split("").map((letter, i) => {
-    return <Blank letter={letter} key={'blank' + i} guessed={props.correct.includes(letter) ? true : false} />
+    return <Blank letter={letter} key={'blank' + i} guessed={correct.includes(letter) ? true : false} />
   }) : null;
 
   const exes = Array.from(Array(6).keys()).map((guess, i) => {
-    return <Ex incorrect={props.incorrect[i]} key={'x' + i}/>
+    return <Ex incorrect={incorrect[i]} key={'x' + i}/>
   });
 
   const handleChangeGuess = e => {
-    props.setGuess(e.target.value)
+    setGuess(e.target.value)
   };
 
   const validGuess = guess => {
     if (guess === "" || !guess.match(/[a-z]/i) || guess.split("").length !== 1) {
-      props.setAlert("Enter a letter");
-      return false
-    } else if (props.incorrect.includes(guess) || props.correct.includes(guess)) {
-      props.setAlert("You have already guessed that letter");
-      return false
+      setAlert("Enter a letter");
+      return false;
+    } else if (incorrect.includes(guess) || correct.includes(guess)) {
+      setAlert("You have already guessed that letter");
+      return false;
     } else {
-      props.setAlert('');
-      return true
+      setAlert('');
+      console.log("valid guess");
+      return true;
     }
   };
 
   const handleSubmitGuess = e => {
     e.preventDefault();
-    if (validGuess(props.guess)) {
-      if (props.word.split("").includes(props.guess)) {
-        props.setCorrect([...props.correct, props.guess]);
-        if (props.word.split("").every(letter=>[...props.correct, props.guess].includes(letter))) {
-          props.setGameState('won')
+    if (validGuess(guess)) {
+      if (props.word.split("").includes(guess)) {
+        setCorrect([...correct, guess]);
+        if (props.word.split("").every(letter=>[...correct, guess].includes(letter))) {
+          setGameOver(true);
+          setAlert("You won!");
         }
       } else {
-        props.setIncorrect([...props.incorrect, props.guess]);
-        if (props.guessesLeft === 1) {
-          props.setGameState('lost')
-        } else {
-          props.setGuessesLeft(props.guessesLeft-1);
+        setIncorrect([...incorrect, guess]);
+        let newGuessesLeft = guessesLeft-1
+        setGuessesLeft(newGuessesLeft);
+        if (newGuessesLeft === 0) {
+          setGameOver(true);
+          setAlert("You lost!");
         }
       }
     }
-    props.setGuess('');
+    setGuess('');
   };
+
+  const resetGame = () => {
+    setGuessesLeft(6);
+    setCorrect([]);
+    setIncorrect([]);
+    setGameOver(false);
+    props.resetApp();
+  }
+
+  console.log(guessesLeft)
+
   return (
     <div>
-      <Alert alert={props.alert}/>
+      <Alert alert={alert}/>
       {blanks}
-      <div className="buttons">
-        <form onSubmit={handleSubmitGuess}>
-          <label>
-            <input
-              type="text"
-              value={props.guess}
-              name="guess"
-              onChange={handleChangeGuess}/>
-          </label>
-          <input type="submit" value="guess" />
-        </form>
-        <button>hint</button>
-        <button>end it</button>
-      </div>
-      {exes}
+      {gameOver ? <button onClick={resetGame}>Play Again</button> : null}
+      {!gameOver ? (
+        <div>
+          <div className="buttons">
+            <form onSubmit={handleSubmitGuess}>
+              <label>
+                <input
+                  type="text"
+                  value={guess}
+                  name="guess"
+                  onChange={handleChangeGuess}/>
+              </label>
+              <input type="submit" value="guess" />
+            </form>
+            <button>hint</button>
+            <button>end it</button>
+          </div>
+          {exes}
+        </div>
+      ) : null}
     </div>
   )
 }
